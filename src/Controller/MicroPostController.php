a75c857c22fece1a956a69947529e668db0fe9fa
Pooja Patel <pp30111991@gmail.com>
2022-11-03 16:17:51 +0100

    Update MicroPostController.php

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -3,23 +3,24 @@
 namespace App\Controller;
 
 use App\Entity\MicroPost;
-use App\Form\MicroPostType;
 use App\Entity\User;
+use App\Form\MicroPostType;
 use App\Repository\MicroPostRepository;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\FormFactoryInterface;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
 /**
-* @Route("/micro-post")
+ * @Route("/micro-post")
  */
 class MicroPostController
 {
@@ -35,42 +36,40 @@ class MicroPostController
      * @var FormFactoryInterface
      */
     private $formFactory;
-    
     /**
-     * @var EntityManagerInterface $entityManager
+     * @var EntityManagerInterface
      */
     private $entityManager;
-    
     /**
-     * @var RouterInterface $router
+     * @var RouterInterface
      */
     private $router;
-    
     /**
-     * @var FlashBagInterface $flashBag
+     * @var FlashBagInterface
      */
     private $flashBag;
-    
     /**
      * @var AuthorizationCheckerInterface
      */
     private $authorizationChecker;
 
-
+    /**
+     * @param \Twig_Environment $twig
+     * @param MicroPostRepository $microPostRepository
+     * @param FormFactoryInterface $formFactory
+     * @param EntityManagerInterface $entityManager
+     * @param RouterInterface $router
+     */
     public function __construct(
-            \Twig\Environment $twig, 
-            MicroPostRepository $microPostRepository,
-            FormFactoryInterface $formFactory,
-            EntityManagerInterface $entityManager,
-            RouterInterface $router,
-            FlashBagInterface $flashBag,
-            AuthorizationCheckerInterface $authorizationChecker
-            
-        ) 
-    {
+        \Twig_Environment $twig, MicroPostRepository $microPostRepository,
+        FormFactoryInterface $formFactory,
+        EntityManagerInterface $entityManager, RouterInterface $router,
+        FlashBagInterface $flashBag,
+        AuthorizationCheckerInterface $authorizationChecker
+    ) {
         $this->twig = $twig;
         $this->microPostRepository = $microPostRepository;
-        $this->formFactory= $formFactory;
+        $this->formFactory = $formFactory;
         $this->entityManager = $entityManager;
         $this->router = $router;
         $this->flashBag = $flashBag;
@@ -80,32 +79,30 @@ class MicroPostController
     /**
      * @Route("/", name="micro_post_index")
      */
-    /* get the Post list By date*/ 
-    /* here call token storage for featch current user because here MicroPostController class not extends
-        Controller class */
-    public function index(TokenStorageInterface $tokenStorage, UserRepository $userRepository) 
-    {  
+    public function index(
+        TokenStorageInterface $tokenStorage, UserRepository $userRepository
+    ) {
         $currentUser = $tokenStorage->getToken()
-                ->getUser();
+            ->getUser();
         $usersToFollow = [];
-        
-        /* Check current user authenticated or not */
-        if($currentUser instanceof User) {
-            /* show following user post only e.g pooja following tirth so here pooja can see tirth's Post*/
-            
-            /* Automatically featch data from database using getFollowing() method */
+
+        if ($currentUser instanceof User) {
             $posts = $this->microPostRepository->findAllByUsers(
-                    $currentUser->getFollowing()
-                );
+                $currentUser->getFollowing()
+            );
+
             $usersToFollow = count($posts) === 0 ?
                 $userRepository->findAllWithMoreThan5PostsExceptUser(
                     $currentUser
                 ) : [];
         } else {
             $posts = $this->microPostRepository->findBy(
-                    [],
-                    ['time'=>'DESC']);
+                [],
+                ['time' => 'DESC']
+            );
         }
+
+
         $html = $this->twig->render(
             'micro-post/index.html.twig',
             [
@@ -113,47 +110,57 @@ class MicroPostController
                 'usersToFollow' => $usersToFollow,
             ]
         );
+
         return new Response($html);
     }
-    
+
     /**
      * @Route("/edit/{id}", name="micro_post_edit")
      * @Security("is_granted('edit', microPost)", message="Access denied")
      */
-    public function edit(MicroPost $microPost, Request $request) 
+    public function edit(MicroPost $microPost, Request $request)
     {
-        //$this->denyUnlessGranted('edit',$microPost);
-//        if(!$this->authorizationChecker->isGranted('edit', $microPost)) {
-//            throw new UnauthorizedHttpException();
-//        }
-        $form = $this->formFactory->create(MicroPostType::class,$microPost);
+        //        $this->denyUnlessGranted('edit', $microPost);
+
+        //        if (!$this->authorizationChecker->isGranted('edit', $microPost)) {
+        //            throw new UnauthorizedHttpException();
+        //        }
+
+        $form = $this->formFactory->create(
+            MicroPostType::class,
+            $microPost
+        );
         $form->handleRequest($request);
-        
-        if($form->isSubmitted() && $form->isValid()) {
-            $this->entityManager->persist($microPost);
+
+        if ($form->isSubmitted() && $form->isValid()) {
             $this->entityManager->flush();
-            
             return new RedirectResponse(
                 $this->router->generate('micro_post_index')
             );
         }
-        
-        $html = $this->twig->render('micro-post/add.html.twig',
-                ['form'=>$form->createView()]
-            );
-        return new Response($html);
+
+        return new Response(
+            $this->twig->render(
+                'micro-post/add.html.twig',
+                ['form' => $form->createView()]
+            )
+        );
     }
+
     /**
      * @Route("/delete/{id}", name="micro_post_delete")
      * @Security("is_granted('delete', microPost)", message="Access denied")
      */
-    public function delete(MicroPost $microPost) 
+    public function delete(MicroPost $microPost)
     {
         $this->entityManager->remove($microPost);
         $this->entityManager->flush();
-        
-        $this->flashBag->add('notice','Micro post was deleted');
-        
+
+        $this->flashBag->add(
+            'notice',
+            'Micro post was deleted'
+        );
+
         return new RedirectResponse(
             $this->router->generate('micro_post_index')
         );
@@ -163,68 +170,70 @@ class MicroPostController
      * @Route("/add", name="micro_post_add")
      * @Security("is_granted('ROLE_USER')")
      */
-    /* For register user can add Post without error if not regiestr then go to login page direct*/
-    public function add(Request $request, TokenStorageInterface $tokenStorage) 
+    public function add(Request $request, TokenStorageInterface $tokenStorage)
     {
-       /* For register user can add Post without error if not regiestr then go to login page direct*/
-        $user = $tokenStorage->getToken()->getUser();
-        
+        $user = $tokenStorage->getToken()
+            ->getUser();
+
         $microPost = new MicroPost();
-        
-        /* This time comment because we set setTimeOnPersist() method in micropost entity. and that method call 
-        automatically date*/
-        //$microPost->setTime(new \DateTime());
+        //        $microPost->setTime(new \DateTime());
         $microPost->setUser($user);
-        
-        $form = $this->formFactory->create(MicroPostType::class,$microPost);
+
+        $form = $this->formFactory->create(
+            MicroPostType::class,
+            $microPost
+        );
         $form->handleRequest($request);
-        
-        if($form->isSubmitted() && $form->isValid()) {
+
+        if ($form->isSubmitted() && $form->isValid()) {
             $this->entityManager->persist($microPost);
             $this->entityManager->flush();
-            
-            return new RedirectResponse($this->router->generate('micro_post_index'));
-        }
-        
-        $html = $this->twig->render('micro-post/add.html.twig',
-                ['form'=>$form->createView()]
+
+            return new RedirectResponse(
+                $this->router->generate('micro_post_index')
             );
-        return new Response($html);
+        }
+
+        return new Response(
+            $this->twig->render(
+                'micro-post/add.html.twig',
+                ['form' => $form->createView()]
+            )
+        );
     }
-   
+
     /**
      * @Route("/user/{username}", name="micro_post_user")
      */
-    /* user wise show post click on username show all post them*/
-    public function userPosts(User $userWithPosts) 
+    public function userPosts(User $userWithPosts)
     {
         $html = $this->twig->render(
             'micro-post/user-posts.html.twig',
             [
-                /* Both method we can do it  this one First */
                 'posts' => $this->microPostRepository->findBy(
                     ['user' => $userWithPosts],
-                    ['time'=>'DESC']
+                    ['time' => 'DESC']
                 ),
                 'user' => $userWithPosts,
-//               /* Second 2*/ 
-                //'posts' =>$userWithPosts->getPosts(),
+                //'posts' => $userWithPosts->getPosts()
             ]
         );
+
         return new Response($html);
     }
+
     /**
      * @Route("/{id}", name="micro_post_post")
      */
-    /*get the individual post by id*/ 
     public function post(MicroPost $post)
     {
-        //$post = $this->microPostRepository->find($id);
-        $html = $this->twig->render('micro-post/post.html.twig',
+        return new Response(
+            $this->twig->render(
+                'micro-post/post.html.twig',
                 [
-                    'post' => $post
+                    'post' => $post,
                 ]
-            );        
-        return new Response($html);
+            )
+        );
     }
 }
